ea7e88e5c6d8f92c8e551df60230497b2c6357f7
Angela <yoonsso99@naver.com>
2026-01-20 22:34:01 -0800

    feat: add edit listing functionality and mood filter synchronization

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
-//dummy data
-const listings = [
+let listings = JSON.parse(localStorage.getItem('listings')) || [
     {
         id: 1,
         title: "Home Name",
@@ -9,7 +8,8 @@ const listings = [
         neighborhood: "metrotown",
         mood: "cozy ocean",
         location: { lat: 49.2276, lng: -122.9992 },
-        img: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?q=80&w=400"
+        img: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?q=80&w=400",
+        owner: "onyourMark"
     },
     {
         id: 2,
@@ -19,7 +19,8 @@ const listings = [
         neighborhood: "downtown",
         mood: "modern city",
         location: { lat: 49.2848, lng: -123.1215 },
-        img: "https://images.unsplash.com/photo-1448630360428-65456885c650?q=80&w=400"
+        img: "https://images.unsplash.com/photo-1448630360428-65456885c650?q=80&w=400",
+        owner: "gelatto"
     },
     {
         id: 3,
@@ -29,7 +30,8 @@ const listings = [
         neighborhood: "joyce-collingwood",
         mood: "nature wood",
         location: { lat: 49.2384, lng: -123.0318 },
-        img: "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?q=80&w=400"
+        img: "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?q=80&w=400",
+        owner: "samoyad"
     },
     {
         id: 4,
@@ -39,7 +41,8 @@ const listings = [
         neighborhood: "kitsilano",
         mood: "ocean chic",
         location: { lat: 49.2684, lng: -123.1583 },
-        img: "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?q=80&w=400"
+        img: "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?q=80&w=400",
+        owner: "onyourMark"
     },
     {
         id: 5,
@@ -49,7 +52,8 @@ const listings = [
         neighborhood: "north vancouver",
         mood: "mountain cool",
         location: { lat: 49.3200, lng: -123.0724 },
-        img: "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?q=80&w=400"
+        img: "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?q=80&w=400",
+        owner: "samoyad"
     },
     {
         id: 6,
@@ -59,7 +63,8 @@ const listings = [
         neighborhood: "burnaby central",
         mood: "park nature",
         location: { lat: 49.2285, lng: -123.0076 },
-        img: "https://images.unsplash.com/photo-1560448204-e02f11c3d0e2?q=80&w=400"
+        img: "https://images.unsplash.com/photo-1560448204-e02f11c3d0e2?q=80&w=400",
+        owner: "gelatto"
     },
     {
         id: 7,
@@ -69,7 +74,8 @@ const listings = [
         neighborhood: "gastown",
         mood: "modern cute",
         location: { lat: 49.2827, lng: -123.1053 },
-        img: "https://images.unsplash.com/photo-1493809842364-78817add7ffb?q=80&w=400"
+        img: "https://images.unsplash.com/photo-1493809842364-78817add7ffb?q=80&w=400",
+        owner: "samoyad"
     }
 ];
 
@@ -92,8 +98,8 @@ window.initMap = function () {
         { featureType: "water", elementType: "labels.text.stroke", stylers: [{ color: "#17263c" }] }
     ];
 
-    const isMain2 = document.getElementById('main-2').classList.contains('hidden');
-    const targetId = isMain2 ? "#map2" : "map1";
+    const isMain2 = !document.getElementById('main-2').classList.contains('hidden');
+    const targetId = isMain2 ? "#map2" : "#map1";
     const mapElement = document.querySelector(targetId);
 
     if (!mapElement) return;
@@ -140,6 +146,29 @@ function renderListings(data) {
         `;
         container.innerHTML += cardHTML;
     });
+
+    const user = JSON.parse(localStorage.getItem('user'));
+    const myItems = listings.filter(item => item.owner === user.userId);
+    const mycontainer = document.querySelector('.rent-container');
+    mycontainer.innerHTML = "";
+    if (myItems) {
+        myItems.forEach(rent => {
+            const cardHTML = `
+            <div class="rent-detail" onclick="openEditModal(${rent.id})"> 
+            <img class="rent-img" src="${rent.img}">
+            <p class="rent-name">${rent.title} (${rent.sqft} sq ft)</p>
+            <p class="rent-price">$ ${rent.price} / month</p>
+            <p class="rent-neighborhood">${rent.neighborhood}</p>
+            <p class="rent-filter">${rent.mood}</p>
+            <div class="btn-option">
+                <button class="rent-update">🩵</button>
+                <button class="rent-remove">❌</button>
+            </div>
+        </div>
+            `;
+            mycontainer.innerHTML += cardHTML;
+        });
+    }
 }
 
 const navBtn = document.querySelector('.profile-arrow')
@@ -246,10 +275,14 @@ function login() {
 
     if (inputID.value === user.userId && inputPW.value === user.userPw) {
         alert(`Welcome! ${user.userName}`);
-        switchTab('main-1');
+
+        //switchTab('main-1');
         //test
         // renderListings(listings);
         // switchTab('main-2');
+        switchTab('myrentlist');
+        renderListings(listings);
+
 
         setTimeout(() => {
             initMap();
@@ -293,4 +326,161 @@ if (logoutBtn) {
         document.querySelector('#login-pw').value = "";
     });
 }
+
+const newRentBtn = document.querySelector('#new-btn');
+const newRentModal = document.querySelector('#new-rent-modal');
+const saveNewRent = document.querySelector('#create-new-btn');
+const cancelNewRent = document.querySelector('#cancel-new-btn');
+
+if (newRentBtn) {
+    newRentBtn.addEventListener('click', () => {
+        newRentModal.classList.remove('hidden');
+    });
+}
+
+if (cancelNewRent) {
+    cancelNewRent.addEventListener('click', () => {
+        newRentModal.classList.add('hidden');
+    });
+}
+
+const tags = document.querySelectorAll('#new-rent-modal .filter-tag');
+
+tags.forEach(tag => {
+    tag.addEventListener('click', () => {
+        tag.classList.toggle('selected');
+    });
+});
+
+const editTags = document.querySelectorAll('#edit-rent-modal .filter-tag');
+
+editTags.forEach(tag=>{
+    tag.addEventListener('click',()=>{
+        tag.classList.toggle('selected');
+    });
+});
+
+function createListing() {
+
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    if (!user) {
+        alert("Please login first!!");
+        return;
+    }
+
+    const imgValue = "https://images.unsplash.com/photo-1518780664697-55e3ad937233?q=80&w=400";
+
+    const nameValue = document.querySelector('#new-name').value;
+    const sqftValue = Number(document.querySelector('#new-sqft').value);
+    const priceValue = Number(document.querySelector('#new-price').value);
+    const neighValue = document.querySelector('#new-neigh').value;
+
+    const selectedTags = document.querySelectorAll('#new-rent-modal .filter-tag.selected');
+    const selectedTagsArray = Array.from(selectedTags);
+    const moodValues = selectedTagsArray.map(tag => tag.textContent);
+
+    const newItem = {
+        id: listings.length + 1,
+        title: nameValue,
+        price: priceValue,
+        sqft: sqftValue,
+        neighborhood: neighValue,
+        mood: moodValues.join(' '),
+        location: { lat: 49.2, lng: -123.1 },
+        img: imgValue,
+        owner: user.userId
+    }
+
+    listings.push(newItem);
+    localStorage.setItem('listings', JSON.stringify(listings));
+
+    renderListings(listings);
+    initMap();
+
+    document.querySelector('#new-rent-modal').classList.add('hidden');
+    document.querySelector('#new-name').value = "";
+    document.querySelector('#new-sqft').value = "";
+    document.querySelector('#new-price').value = "";
+    document.querySelector('#new-neigh').value = "";
+
+    selectedTags.forEach(tags => tags.classList.remove('selected'));
+}
+
+if (saveNewRent) {
+    saveNewRent.addEventListener('click', () => {
+        alert("New Rent Registrated!");
+        createListing();
+    });
+}
+
+const editRentModal = document.querySelector('#edit-rent-modal');
+const editCancelBtn = document.querySelector('#cancel-edit-btn');
+const saveEditBtn = document.querySelector('#create-edit-btn');
+
+if (editCancelBtn) {
+    editCancelBtn.addEventListener('click', () => {
+        editRentModal.classList.add('hidden');
+    });
+}
+
+let currentEditId = null;
+
+function openEditModal(id) {
+    const user = JSON.parse(localStorage.getItem('user'));
+    const target = listings.find(item => item.id === id);
+
+    if (target.owner !== user.userId) {
+        alert("No right edit this!");
+        return;
+    }
+
+    currentEditId = id;
+    console.log('id : ' + id);
+
+    document.querySelector('#edit-name').value = target.title;
+    document.querySelector('#edit-sqft').value = target.sqft;
+    document.querySelector('#edit-price').value = target.price;
+    document.querySelector('#edit-neigh').value = target.neighborhood;
+
+    const editTags = document.querySelectorAll('#edit-rent-modal .filter-tag');
+
+    editTags.forEach(tag => {
+        if (target.mood.includes(tag.textContent)) {
+            tag.classList.add('selected');
+        } else {
+            tag.classList.remove('selected');
+        }
+    });
+
+    editRentModal.classList.remove('hidden');
+
+}
+
+function saveEdit() {
+    const index = listings.findIndex(item => item.id === currentEditId);
+    const editSelectedTags = document.querySelectorAll('#edit-rent-modal .filter-tag.selected');
+    const editSelectedTagsArray = Array.from(editSelectedTags);
+    const eidtMoodValues = editSelectedTagsArray.map(tag => tag.textContent);
+    
+    listings[index] = {
+        ...listings[index],
+        title: document.querySelector('#edit-name').value,
+        price: Number(document.querySelector('#edit-price').value),
+        sqft: Number(document.querySelector('#edit-sqft').value),
+        neighborhood: document.querySelector('#edit-neigh').value,
+        mood: eidtMoodValues.join(' ')
+    };
+
+    localStorage.setItem('listings', JSON.stringify(listings));
+
+    renderListings(listings);
+    editRentModal.classList.add('hidden');
+    alert("Edit completed!");
+}
+
+if (saveEditBtn) {
+    saveEditBtn.addEventListener('click', saveEdit);
+}
+
 switchTab('login-page');
